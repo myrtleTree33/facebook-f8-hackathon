14ee46a909a4f8d10d2561a5db16f4aee3abf638
joowon <saven7788@gmail.com>
2019-04-28 21:08:37 -0700

    make some echo bot

--- backend/src/routes/pageRoutes.js
+++ backend/src/routes/pageRoutes.js
@@ -53,18 +53,84 @@ routes.get('/webhook', (req, res, next) => {
   }
   res.send('wrong token');
 });
+app.post("/webhook", function(req, res) {
+  console.log("WEBHOOK GET IT WORKS");
+  var data = req.body;
+  console.log(data);
 
-routes.post('/webhook', (req, res) => {
-  const { body } = req;
-  console.log(`Received payload=${JSON.stringify(body)}`);
+  // Make sure this is a page subscription
+  if (data.object == 'page') {
+      // Iterate over each entry
+      // There may be multiple if batched
+      data.entry.forEach(function(pageEntry) {
+          var pageID = pageEntry.id;
+          var timeOfEvent = pageEntry.time;
 
-  if (body.object === 'page') {
-    // TODO: Find and do something to the event page
-    // TODO: save user pid
-    res.send('EVENT_RECEIVED');
-  } else {
-    res.sendStatus(404);
+          // Iterate over each messaging event
+          pageEntry.messaging.forEach(function(messagingEvent) {
+              if (messagingEvent.optin) {
+                  receivedAuthentication(messagingEvent);
+              } else if (messagingEvent.message) {
+                  receivedMessage(messagingEvent);
+              } else if (messagingEvent.postback) {
+                  receivedPostback(messagingEvent);
+              } else {
+                  console.log("Webhook received unknown messagingEvent: ", messagingEvent);
+              }
+          });
+      });
+
+      res.sendStatus(200);
   }
 });
+
+function receivedMessage(event) {
+  var senderId = event.sender.id;
+  var content = event.message.text;
+  var echo_message = "ECHO : " + content;
+  sendTextMessage(senderId, echo_message);
+}
+
+function receivedPostback(event) {
+  console.log("RECEIVED POSTBACK IT WORKS");
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfPostback = event.timestamp;
+
+  var payload = event.postback.payload;
+
+  console.log("Received postback for user %d and page %d with payload '%s' " +
+      "at %d", senderID, recipientID, payload, timeOfPostback);
+
+  sendTextMessage(senderID, "Postback called");
+}
+
+function sendTextMessage(recipientId, message) {
+  request({
+      url: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: { access_token: PAGE_ACCESS_TOKEN },
+      method: 'POST',
+      json: {
+          recipient: { id: recipientId },
+          message: { text: message }
+      }
+  }, function(error, response, body) {
+      if (error) {
+          console.log('Error sending message: ' + response.error);
+      }
+  });
+}
+// routes.post('/webhook', (req, res) => {
+//   const { body } = req;
+//   console.log(`Received payload=${JSON.stringify(body)}`);
+
+//   if (body.object === 'page') {
+//     // TODO: Find and do something to the event page
+//     // TODO: save user pid
+//     res.send('EVENT_RECEIVED');
+//   } else {
+//     res.sendStatus(404);
+//   }
+// });
 
 export default routes;
