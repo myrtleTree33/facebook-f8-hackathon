ca3f0e8a85c0ef093bc872f112a647f0e7e3554c
joowon <saven7788@gmail.com>
2019-04-29 16:41:12 -0700

    making try catch, async  in axios

--- backend/src/services/initChatbot.js
+++ backend/src/services/initChatbot.js
@@ -9,41 +9,43 @@ const makeGreeting = () =>{
 
 }
 
-const makePersistantMenu = () =>{
+const makePersistantMenu = async () => {
     console.log("makePersistantMenu");
-    axios.post('https://graph.facebook.com/v2.6/me/messenger_profile', {
-        params: {
-            access_token: PAGE_ACCESS_TOKEN
-        },
-        data : {
-            "persistent_menu":[
-                {
-                    "locale":"en_US",
-                    "composer_input_disabled": true,
-                    "call_to_actions":[
-                        {
-                            "type":"web_url",
-                            "title": "go trapple rabbit",
-                            "url":"http://blog.kebhana.com/990",
-                            "webview_height_ratio": "tall"
-                        },{
-                            "type":"web_url",
-                            "title": "test2",
-                            "url":"http://blog.kebhana.com/990",
-                            "webview_height_ratio": "tall"
-                        },{
-                            "type":"web_url",
-                            "title": "test2",
-                            "url":"http://blog.kebhana.com/990",
-                            "webview_height_ratio": "tall"
-                        }
-                    ]
-                }
-            ]
-        },
-    }).then(response => {
-        console.log('response', response);
-    });
+    try {
+        await axios.post( 
+            `https://graph.facebook.com/v3.2/me/messages?access_token=${PAGE_ACCESS_TOKEN}`,
+            {
+                "persistent_menu":[
+                    {
+                        "locale":"en_US",
+                        "composer_input_disabled": true,
+                        "call_to_actions":[
+                            {
+                                "type":"web_url",
+                                "title": "go trapple rabbit",
+                                "url":"http://blog.kebhana.com/990",
+                                "webview_height_ratio": "tall"
+                            },{
+                                "type":"web_url",
+                                "title": "test2",
+                                "url":"http://blog.kebhana.com/990",
+                                "webview_height_ratio": "tall"
+                            },{
+                                "type":"web_url",
+                                "title": "test2",
+                                "url":"http://blog.kebhana.com/990",
+                                "webview_height_ratio": "tall"
+                            }
+                        ]
+                    }
+                ]
+            },
+        ).then(response => {
+            console.log('response', response);
+        });
+    } catch (e) {
+        console.log('error', e);
+    }
 }
 
 const initBot = () =>{
