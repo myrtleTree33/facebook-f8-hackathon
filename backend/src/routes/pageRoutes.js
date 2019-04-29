a4f22f721a1669e562db56adca215e2a6c8949b8
jhtong <me@joeltong.org>
2019-04-28 22:02:40 -0700

    fixed quote

--- backend/src/routes/pageRoutes.js
+++ backend/src/routes/pageRoutes.js
@@ -46,7 +46,7 @@ routes.get('/', (req, res) => {
 });
 
 routes.get('/webhook', (req, res, next) => {
-  console.log("webhook test")
+  console.log('webhook test');
   const hubChallenge = req.query['hub.challenge'];
   const verifyToken = req.query['hub.verify_token'];
   if (verifyToken === VERIFY_TOKEN) {
@@ -56,72 +56,80 @@ routes.get('/webhook', (req, res, next) => {
   res.send('wrong token');
 });
 
-routes.post("/webhook/", function(req, res) {
-  console.log("WEBHOOK GET IT WORKS");
+routes.post('/webhook', function(req, res) {
+  console.log('WEBHOOK GET IT WORKS');
   const data = req.body;
   console.log(data);
 
   // Make sure this is a page subscription
   if (data.object == 'page') {
-      // Iterate over each entry
-      // There may be multiple if batched
-      data.entry.forEach(function(pageEntry) {
-          var pageID = pageEntry.id;
-          var timeOfEvent = pageEntry.time;
+    // Iterate over each entry
+    // There may be multiple if batched
+    data.entry.forEach(function(pageEntry) {
+      var pageID = pageEntry.id;
+      var timeOfEvent = pageEntry.time;
 
-          // Iterate over each messaging event
-          pageEntry.messaging.forEach(function(messagingEvent) {
-              if (messagingEvent.optin) {
-                  // receivedAuthentication(messagingEvent);
-              } else if (messagingEvent.message) {
-                  // receivedMessage(messagingEvent);
-              } else if (messagingEvent.postback) {
-                  // receivedPostback(messagingEvent);
-              } else {
-                  console.log("Webhook received unknown messagingEvent: ", messagingEvent);
-              }
-          });
+      // Iterate over each messaging event
+      pageEntry.messaging.forEach(function(messagingEvent) {
+        if (messagingEvent.optin) {
+          // receivedAuthentication(messagingEvent);
+        } else if (messagingEvent.message) {
+          // receivedMessage(messagingEvent);
+        } else if (messagingEvent.postback) {
+          // receivedPostback(messagingEvent);
+        } else {
+          console.log('Webhook received unknown messagingEvent: ', messagingEvent);
+        }
       });
+    });
 
-      res.sendStatus(200);
+    res.sendStatus(200);
   }
 });
 
 function receivedMessage(event) {
   var senderId = event.sender.id;
   var content = event.message.text;
-  var echo_message = "ECHO : " + content;
+  var echo_message = 'ECHO : ' + content;
   sendTextMessage(senderId, echo_message);
 }
 
 function receivedPostback(event) {
-  console.log("RECEIVED POSTBACK IT WORKS");
+  console.log('RECEIVED POSTBACK IT WORKS');
   var senderID = event.sender.id;
   var recipientID = event.recipient.id;
   var timeOfPostback = event.timestamp;
 
   var payload = event.postback.payload;
 
-  console.log("Received postback for user %d and page %d with payload '%s' " +
-      "at %d", senderID, recipientID, payload, timeOfPostback);
+  console.log(
+    "Received postback for user %d and page %d with payload '%s' " + 'at %d',
+    senderID,
+    recipientID,
+    payload,
+    timeOfPostback
+  );
 
-  sendTextMessage(senderID, "Postback called");
+  sendTextMessage(senderID, 'Postback called');
 }
 
 function sendTextMessage(recipientId, message) {
-  request({
+  request(
+    {
       url: 'https://graph.facebook.com/v2.6/me/messages',
       qs: { access_token: PAGE_ACCESS_TOKEN },
       method: 'POST',
       json: {
-          recipient: { id: recipientId },
-          message: { text: message }
+        recipient: { id: recipientId },
+        message: { text: message }
       }
-  }, function(error, response, body) {
+    },
+    function(error, response, body) {
       if (error) {
-          console.log('Error sending message: ' + response.error);
+        console.log('Error sending message: ' + response.error);
       }
-  });
+    }
+  );
 }
 // routes.post('/webhook', (req, res) => {
 //   const { body } = req;
