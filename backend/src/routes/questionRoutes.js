9f3a9eecc5c43955392655d27e7a16c7ab06c375
joowon <saven7788@gmail.com>
2019-04-29 23:31:39 -0700

    check get

--- backend/src/routes/questionRoutes.js
+++ backend/src/routes/questionRoutes.js
@@ -3,12 +3,20 @@ import Qustion from '../models/Question';
 
 const routes = Router();
 
-routes.post('/qustion', (req, res) => {
+routes.post('/question', (req, res) => {
 
     const qustion = req.body;
     Qustion.insert( { qustion } )
 
-    res.json(Qustion.find());
+    // res.json(Qustion.find());
+    res.json({ message: 'question' });
+
+})
+.get('/question', (req, res) => {
+
+
+    res.json({ message: 'question' });
+
 });
 
 export default routes;
