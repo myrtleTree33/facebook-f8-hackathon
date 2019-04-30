b57038322b42e06e56c531502627e4cdec720001
joowon <saven7788@gmail.com>
2019-04-29 19:59:23 -0700

    fix async problem

--- backend/src/services/quickReply.js
+++ backend/src/services/quickReply.js
@@ -1,17 +1,46 @@
 import axios from 'axios';
 
-const answer = (event) =>{
+const answer = async (event) =>{
     console.log('********************');
     console.log("quickReply test")
     console.log('event', JSON.stringify(event, null, 2))
     console.log('********************');
 
-    // const text = 
+    const text = {
+        "attachment":{
+            "type":"template",
+            "payload":{
+                "template_type":"generic",
+                "elements":[
+                    {
+                        "title":"Welcome!",
+                        "image_url":"https://petersfancybrownhats.com/company_image.png",
+                        "subtitle":"We have the right hat for everyone.",
+                        "default_action": {
+                        "type": "web_url",
+                        "url": "https://petersfancybrownhats.com/view?item=103",
+                        "webview_height_ratio": "tall",
+                        },
+                        "buttons":[
+                        {
+                            "type":"web_url",
+                            "url":"https://petersfancybrownhats.com",
+                            "title":"View Website"
+                        },{
+                            "type":"postback",
+                            "title":"Start Chatting",
+                            "payload":"DEVELOPER_DEFINED_PAYLOAD"
+                        }]      
+                    }
+                ]
+            } 
+        } 
+    }   
 
-
+    console.log('check text is here ', text)
   
     try {
-        axios.post(
+        await axios.post(
             `https://graph.facebook.com/v3.2/me/messages?access_token=${PAGE_ACCESS_TOKEN}`,
             {
                 messaging_type: 'RESPONSE',
@@ -26,39 +55,32 @@ const answer = (event) =>{
                             "template_type":"generic",
                             "elements":[
                                 {
-                                "title":"Welcome!",
-                                "image_url":"https://petersfancybrownhats.com/company_image.png",
-                                "subtitle":"We have the right hat for everyone.",
-                                "default_action": {
-                                "type": "web_url",
-                                "url": "https://petersfancybrownhats.com/view?item=103",
-                                "webview_height_ratio": "tall",
-                                },
-                                "buttons":[
-                                {
-                                    "type":"web_url",
-                                    "url":"https://petersfancybrownhats.com",
-                                    "title":"View Website"
-                                },{
-                                    "type":"postback",
-                                    "title":"Start Chatting",
-                                    "payload":"DEVELOPER_DEFINED_PAYLOAD"
-                                }              
-                                ]      
-                            }
+                                    "title":"Welcome!",
+                                    "image_url":"https://petersfancybrownhats.com/company_image.png",
+                                    "subtitle":"We have the right hat for everyone.",
+                                    "default_action": {
+                                    "type": "web_url",
+                                    "url": "https://petersfancybrownhats.com/view?item=103",
+                                    "webview_height_ratio": "tall",
+                                    },
+                                    "buttons":[
+                                    {
+                                        "type":"web_url",
+                                        "url":"https://petersfancybrownhats.com",
+                                        "title":"View Website"
+                                    },{
+                                        "type":"postback",
+                                        "title":"Start Chatting",
+                                        "payload":"DEVELOPER_DEFINED_PAYLOAD"
+                                    }]      
+                                }
                             ]
-                        }
-                    }
+                        } 
+                    } 
                 }
                 
             }
-        ).then((response) => {
-            console.log('response', response);
-        })
-        .catch((error) => {
-            console.log('error', error);
-        });
-
+        )
     } catch (e) {
         console.log('error in quickReply', e);
     }
