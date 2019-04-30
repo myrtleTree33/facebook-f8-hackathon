d4733509c2519ee32a75f69e4097537dae32201f
jhtong <me@joeltong.org>
2019-04-30 00:26:11 -0700

    commented out bot sdk

--- backend/src/services/botSdk.js
+++ backend/src/services/botSdk.js
@@ -1,7 +1,7 @@
 import Question from '../models/Question';
 
-async function askQuestion({ userId, maxNum = 3 }) {
-  const question = Question.findOne({}).limit(maxNum);
+// async function askQuestion({ userId, maxNum = 3 }) {
+//   const question = Question.findOne({}).limit(maxNum);
 
-  const { question } = req.body;
-}
+//   const { question } = req.body;
+// }
