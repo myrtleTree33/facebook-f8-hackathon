dc3b6dbdf4fa2e73a12e9bf3539ae4b513cd5782
joowon <saven7788@gmail.com>
2019-04-29 19:52:25 -0700

    the text  get into the axios

--- backend/src/services/quickReply.js
+++ backend/src/services/quickReply.js
@@ -6,37 +6,7 @@ const answer = (event) =>{
     console.log('event', JSON.stringify(event, null, 2))
     console.log('********************');
 
-    const text = {
-        "attachment":{
-            "type":"template",
-            "payload":{
-              "template_type":"generic",
-              "elements":[
-                 {
-                  "title":"Welcome!",
-                  "image_url":"https://petersfancybrownhats.com/company_image.png",
-                  "subtitle":"We have the right hat for everyone.",
-                  "default_action": {
-                    "type": "web_url",
-                    "url": "https://petersfancybrownhats.com/view?item=103",
-                    "webview_height_ratio": "tall",
-                  },
-                  "buttons":[
-                    {
-                      "type":"web_url",
-                      "url":"https://petersfancybrownhats.com",
-                      "title":"View Website"
-                    },{
-                      "type":"postback",
-                      "title":"Start Chatting",
-                      "payload":"DEVELOPER_DEFINED_PAYLOAD"
-                    }              
-                  ]      
-                }
-              ]
-            }
-        }
-    }
+    // const text = 
 
 
   
@@ -49,8 +19,38 @@ const answer = (event) =>{
                     id: event.sender.id
                 },
                 message: {
-                    text
+                    
+                    "attachment":{
+                        "type":"template",
+                        "payload":{
+                            "template_type":"generic",
+                            "elements":[
+                                {
+                                "title":"Welcome!",
+                                "image_url":"https://petersfancybrownhats.com/company_image.png",
+                                "subtitle":"We have the right hat for everyone.",
+                                "default_action": {
+                                "type": "web_url",
+                                "url": "https://petersfancybrownhats.com/view?item=103",
+                                "webview_height_ratio": "tall",
+                                },
+                                "buttons":[
+                                {
+                                    "type":"web_url",
+                                    "url":"https://petersfancybrownhats.com",
+                                    "title":"View Website"
+                                },{
+                                    "type":"postback",
+                                    "title":"Start Chatting",
+                                    "payload":"DEVELOPER_DEFINED_PAYLOAD"
+                                }              
+                                ]      
+                            }
+                            ]
+                        }
+                    }
                 }
+                
             }
         ).then((response) => {
             console.log('response', response);
